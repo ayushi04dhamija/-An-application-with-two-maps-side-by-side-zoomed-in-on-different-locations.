05b49a0c3c229c54cbc54ffd86188d340ce26fe0
Ayushi <ayushi773478@gmail.com>
2018-07-19 16:22:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -76,6 +76,8 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 250, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(30.3517681, 76.7652926 ));
 
 	}
 
@@ -84,6 +86,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
